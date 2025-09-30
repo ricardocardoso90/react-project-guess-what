a7c8508dd1f586eaca86b1a3f02e08968dc7f092
Ricardo Cardoso <ricardomanolo90@gmail.com>
2025-09-30 16:43:35 -0300

    Update commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import { Header } from "./components/Header";
 export default function App() {
   return (
     <div className={styles.container}>
-      <Header />
+      <main>
+        <Header />
+      </main>
     </div>
   )
 };
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,22 @@
-import logo from "../../assets/logo.png";
 import styles from "./styles.module.css";
+
+import logo from "../../assets/logo.png";
+import restart from "../../assets/restart.svg";
 
 export function Header() {
   return (
     <div className={styles.container}>
-      <img src={logo} alt="" />
+      <img src={logo} alt="Logo" />
+
+      <header>
+        <span>
+          <strong>5</strong> de 10 tentativas
+        </span>
+
+        <button type="button">
+          <img src={restart} alt="Reiniciar" />
+        </button>
+      </header>
     </div>
   )
 };
