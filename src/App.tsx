7fb3107d2da532774db31fa3fdb3753fd11abc8f
Ricardo Cardoso <ricardomanolo90@gmail.com>
2025-10-01 00:36:15 -0300

    update commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,18 @@ import styles from "./App.module.css";
 import { Header } from "./components/Header";
 
 export default function App() {
+  function handleRestart() {
+    console.log("Reiniciar");
+  };
+
   return (
     <div className={styles.container}>
       <main>
-        <Header />
+        <Header
+          max={10}
+          current={5}
+          onRestart={handleRestart}
+        />
       </main>
     </div>
   )
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,17 +3,26 @@ import styles from "./styles.module.css";
 import logo from "../../assets/logo.png";
 import restart from "../../assets/restart.svg";
 
-export function Header() {
+type Props = {
+  max: number;
+  current: number;
+  onRestart: () => void;
+};
+
+export function Header({ max, current, onRestart }: Props) {
   return (
     <div className={styles.container}>
       <img src={logo} alt="Logo" />
 
       <header>
         <span>
-          <strong>5</strong> de 10 tentativas
+          <strong>{current}</strong> de {max} tentativas
         </span>
 
-        <button type="button">
+        <button
+          type="button"
+          onClick={onRestart}
+        >
           <img src={restart} alt="Reiniciar" />
         </button>
       </header>
