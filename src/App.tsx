9594f70f0f9fdfc0c1ed267cbc7732ae0f7827f6
Ricardo Cardoso <ricardomanolo90@gmail.com>
2025-10-02 16:30:52 -0300

    update commit

--- src/App.tsx
+++ src/App.tsx
@@ -20,11 +20,13 @@ export default function App() {
 
         <Tip tip="Uma das linguagens de programação mais utilizadas no mundo!!" />
 
-        <Letter value="R" />
-        <Letter value="E" />
-        <Letter value="A" />
-        <Letter value="C" />
-        <Letter value="T" />
+        <div className={styles.word}>
+          <Letter value="R" />
+          <Letter value="E" />
+          <Letter value="A" />
+          <Letter value="C" />
+          <Letter value="T" />
+        </div>
       </main>
     </div>
   )
