be14d551d97685a0e92b7ff7883acf24610ab769
Ricardo Cardoso <ricardomanolo90@gmail.com>
2025-10-07 17:23:02 -0300

    update commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import styles from "./App.module.css";
 
 import { useEffect, useState } from "react";
 import { WORDS, type Challenge } from "./utils/words";
-import { LettersUsed, type LettersUsedProps } from "./components/LettersUsed";
 
 import { Tip } from "./components/Tip";
 import { Header } from "./components/Header";
 import { Letter } from "./components/Letter";
 import { Input } from "./components/Input";
 import { Button } from "./components/Button";
+import { LettersUsed, type LettersUsedProps } from "./components/LettersUsed";
 
 export default function App() {
   const [letter, setLetter] = useState("");
