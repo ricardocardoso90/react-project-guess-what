15efd36d21ac2919fdc3595a1ac958202710c9e9
Ricardo Cardoso <ricardomanolo90@gmail.com>
2025-10-07 15:11:06 -0300

    update commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import styles from "./App.module.css";
 
+import { useEffect, useState } from "react";
+import { WORDS, type Challenge } from "./utils/words";
+
 import { Tip } from "./components/Tip";
 import { Header } from "./components/Header";
 import { Letter } from "./components/Letter";
@@ -8,29 +11,49 @@ import { Button } from "./components/Button";
 import { LettersUsed } from "./components/LettersUsed";
 
 export default function App() {
+  const [letter, setLetter] = useState("");
+  const [attempts, setAttempts] = useState(5);
+  const [challenge, setChallenge] = useState<Challenge | null>(null);
+
   function handleRestart() {
     console.log("Reiniciar");
   };
+
+  function startGame() {
+    const index = Math.floor(Math.random() * WORDS.length);
+    const randomWord = WORDS[index];
+
+    setChallenge(randomWord);
+    setAttempts(0);
+    setLetter("");
+  };
+
+  useEffect(() => {
+    startGame();
+  }, []);
+
+  if (!challenge) return;
 
   return (
     <div className={styles.container}>
       <main>
         <Header
           max={10}
-          current={5}
+          current={attempts}
           onRestart={handleRestart}
         />
 
         <Tip
-          tip="Uma das linguagens de programação mais utilizadas no mundo!!"
+          tip={challenge?.tip}
         />
 
         <div className={styles.word}>
-          <Letter value="R" />
-          <Letter value="E" />
-          <Letter value="A" />
-          <Letter value="C" />
-          <Letter value="T" />
+          {challenge.word.split("").map((item, id) => (
+            <Letter
+              key={id}
+              value={item}
+            />
+          ))}
         </div>
 
         <h4>Palpite</h4>
